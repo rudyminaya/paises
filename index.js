8e0528d170ec8e235777931b82a1acdc445c53b7
Rudy Minaya <rudy.minaya@copstone.net>
2021-02-13 12:22:50 -0500

    se desarrolló el botón en el top para mostrar todos los países

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const renderPaises = (paises) => {
+const renderPaises = (paises, mostrar) => {
+    let valorFinal = 0
+    let valorInicial = 0
+    if (mostrar) {
+        valorInicial = 0
+        valorFinal = paises.length
+    } else {
+        valorFinal = Math.floor(Math.random() * (paises.length - 12) + 12)
+        valorInicial = valorFinal - 12
+    }
     let paisesContent = document.getElementById('contenedor')
-    let valorFinal = Math.floor(Math.random() * (paises.length - 12) + 12)
-    let valorInicial = valorFinal - 12
+    paisesContent.innerHTML = ''
 
     paises.slice(valorInicial, valorFinal).forEach((pais) => {
         let card = document.createElement('div')
@@ -27,26 +35,38 @@ const renderPaises = (paises) => {
         shadow.appendChild(alphaCode)
         bandera.src = pais.flag
         shadow.appendChild(bandera)
-        nombrePais.innerText = pais.name
+        nombrePais.innerText = pais.nativeName
         shadow.appendChild(nombrePais)
         parrafo.innerText = 'Su capital es:'
         shadow.appendChild(parrafo)
         nombreCapital.innerText = pais.capital
         shadow.appendChild(nombreCapital)
-        poblacion.innerText = `${pais.name} tiene una población aproximada de ${pais.population} personas`
+        poblacion.innerText = `${pais.nativeName} tiene una población aproximada de ${pais.population} personas`
         shadow.appendChild(poblacion)
         paisesContent.appendChild(card)
     })
 }
 
-const todos = (e) => {
-    console.log(e, 'funcionaaaa')
-}
+const obtenerPaises = async (idioma) => {
+    let lang = ''
+    let mostrar = false
+    if (idioma === 'es') {
+        lang = 'https://restcountries.eu/rest/v2/lang/es'
+        mostrar = false
+    } else {
+        lang = 'https://restcountries.eu/rest/v2/'
+        mostrar = true
+    }
 
-const obtenerPaises = async () => {
-    fetch('https://restcountries.eu/rest/v2/lang/es')
+    fetch(lang)
         .then((res) => res.json())
-        .then(renderPaises)
+        .then((paises) => {
+            renderPaises(paises, mostrar)
+        })
 }
 
-obtenerPaises()
+obtenerPaises('es')
+
+document.getElementById('mostrarTodo').addEventListener('click', () => {
+    obtenerPaises()
+})
